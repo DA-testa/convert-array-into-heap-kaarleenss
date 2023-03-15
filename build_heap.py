fa69d73e98b5590d05ce08dfad69dfdf562122e8
Kārlis Sīmanis <93652893+kaarleenss@users.noreply.github.com>
2023-03-15 12:19:01 +0000

    ver2.8.3

--- build_heap.py
+++ build_heap.py
@@ -13,18 +13,7 @@ def build_heap(data, n):
                     break
         except IndexError:
             pass
-        for a in range(n, 1, -1):
-        # print(i)
-        try:
-            while data[(a // 2) - 1] > data[a - 1]:
-                data[(a // 2) - 1], data[a - 1] = data[a - 1], data[(a // 2) - 1]
-                swaps.append(((a // 2) - 1, a - 1))
-                a = a // 2
-                if a <= 1:
-                    break
-        except IndexError:
-            pass
-
+        
     return swaps
 
 
@@ -36,14 +25,14 @@ def main():
     burts = input()
     data = []
     n = 0
-    if burts == "I":
+    if burts[0] == "I":
         # first two tests are from keyboard, third test is from a file
 
         # input from keyboard
         n = int(input())
         data = list(map(int, input().split()))
 
-    elif burts == "F":
+    elif burts[0] == "F":
         name = input()
         path = "./tests/" + name
         with open(path, "r") as file:
